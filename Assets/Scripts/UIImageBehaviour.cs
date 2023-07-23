b237bc49c2d2703acb9e23d608f7fd95af791c1a
Xabier <86005264+xaju5@users.noreply.github.com>
2023-07-23 19:16:36 +0200

    Module Creation

---
+++ Assets/Scripts/UIImageBehaviour.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIImageBehaviour : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private float UIBorderPosition;
+    [SerializeField]
+    private GameObject StationModule;
+
+    private bool canSpawnModule;
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        this.initialPosition = transform.position;
+        this.canSpawnModule = true;
+    }
+    void Update()
+    {
+        CheckOutOfBorder();
+    }
+
+    public void OnBeginDrag(PointerEventData _EventData)
+    {
+        //Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData _EventData)
+    {
+        MoveUIItem();
+    }
+
+    public void OnEndDrag(PointerEventData _EventData)
+    {
+        this.canSpawnModule = true;
+    }
+
+    private void MoveUIItem()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    private void CheckOutOfBorder()
+    {
+        if (transform.position.x >= this.UIBorderPosition) return;
+        
+        SpawnModule();
+        transform.position = this.initialPosition;
+        
+    }
+
+    private void SpawnModule()
+    {
+        if (!this.canSpawnModule) return;
+        
+        Vector3 mousePositionOnScreen = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+        Instantiate(this.StationModule, mousePositionOnScreen, Quaternion.identity);
+        this.canSpawnModule = false;
+    }
+}
