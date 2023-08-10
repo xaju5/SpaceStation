1903fd78e88bb05a3719887e1da31d919ad39e54
Xabier <86005264+xaju5@users.noreply.github.com>
2023-08-11 00:42:31 +0200

    Power cost

--- Assets/Scripts/UIImageBehaviour.cs
+++ Assets/Scripts/UIImageBehaviour.cs
@@ -9,18 +9,27 @@ public class UIImageBehaviour : MonoBehaviour, IBeginDragHandler, IDragHandler, 
     private float UIBorderPosition;
     [SerializeField]
     private GameObject StationModule;
+    [SerializeField]
+    private float cost;
+    [SerializeField]
+    private bool generateEnergy;
 
     private bool canSpawnModule;
+    private bool canAffordModule;
     private Vector3 initialPosition;
+    private PowerManager powerManager;
 
     void Start()
     {
+        powerManager = GameObject.Find("PowerManager").GetComponent<PowerManager>();
         this.initialPosition = transform.position;
         this.canSpawnModule = true;
+        this.canAffordModule = true;
     }
     void Update()
     {
         CheckOutOfBorder();
+        CheckAfford();
     }
 
     public void OnBeginDrag(PointerEventData _EventData)
@@ -46,20 +55,43 @@ public class UIImageBehaviour : MonoBehaviour, IBeginDragHandler, IDragHandler, 
     private void CheckOutOfBorder()
     {
         if (transform.position.x >= this.UIBorderPosition) return;
-        
+
         SpawnModule();
         transform.position = this.initialPosition;
-        
+
     }
 
     private void SpawnModule()
     {
         if (!this.canSpawnModule) return;
+        if (!this.canAffordModule) return;
 
         Vector3 mousePositionOnScreen = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
         GameObject module = Instantiate(this.StationModule, mousePositionOnScreen, Quaternion.identity);
         //TODO: Make the instantiated object to drag
         //module.GetComponent<ModuleBehaviour>().ModuleSelection();
         this.canSpawnModule = false;
+        ChangePower();
+    }
+
+    private void CheckAfford()
+    {
+        if(generateEnergy){
+            this.canAffordModule = true;
+        }
+        else{
+            this.canAffordModule = powerManager.CanAfford(this.cost);
+        }
+    }
+    private void ChangePower()
+    {
+        if (generateEnergy)
+        {
+            powerManager.IncreasePower(this.cost);
+        }
+        else
+        {
+            powerManager.DecreasePower(this.cost);
+        }
     }
 }
